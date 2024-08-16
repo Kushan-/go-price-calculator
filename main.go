7d7563471648369541f278c20cff9bec990d4c8d
Kushan- <kushan.joshi@outlook.com>
2024-08-16 12:23:16 -0400

    basic structure

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	prices := []float64{10.0, 20.0, 30.0}
+	taxRates := []float64{0, 0.07, 0.1, 0.15}
+
+	var result = make(map[float64][]float64)
+
+	for _, taxRate := range taxRates {
+		taxIncludedPrices := make([]float64, len(prices))
+		for priceIndex, price := range prices {
+			taxIncludedPrices[priceIndex] = price * (1 + taxRate)
+		}
+		result[taxRate] = taxIncludedPrices
+	}
+
+	fmt.Println("result=>", result)
+}
